73bf2cdc219aee830ef504494aac6f0e9ab026e1
Laura Corbi Villalba <lcorbivi@everis.com>
2019-05-19 21:44:53 +0200

    working with sample

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const http = require('http');
-const open = require('open');
+const Open = require('open');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const Start = require('./lib/start.js')
 
 const app = express();
 const port = 3000;
@@ -14,10 +14,12 @@ app.get('/', function(req, res){
 });
 
 app.post('/start', function(req, res){
- res.send(req.body)
+  let start = new Start(req.body.value)
+  let response = start.returnRes();
+  res.send(response)
 })
    
 app.listen(port, '127.0.0.1',function(){
     console.log('Launching the browser!');
-    open(`http://127.0.0.1:${port}`);
+    Open(`http://127.0.0.1:${port}`);
 });
--- public/main.js
+++ public/main.js
@@ -1,18 +1,64 @@
+let startButton = document.getElementById('start');
+let input = document.getElementById('input');
+let output = document.getElementById('output');
 
-let x = {    
-    "test": "test"
-}
-let startButton = document.getElementById('start')
+
+// let inputSample = `5 3
+// 1 1 E
+// RFRFRFRF
+// 3 2 N
+// FRRFLLFFRRFLL
+// 0 3 W
+// LLFFFLFLFL`;
+// let inputSample=
+// {
+//     "value": "5 3\n1 1 E\nRFRFRFRF\n3 2 N \nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL"
+// }
+
+let inputSample = {value: '5 3\n1 1 E\nRFRFRFRF\n3 2 N \nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL'}
+
+input.value = inputSample.value;
 
 
 startButton.addEventListener("click", function(){
+    // let game = newGame(input.value);
+    // console.log(game)
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           console.log(this.response)
+          let response = xhttp.responseText;
+          output.value = response;
         }
       };
     xhttp.open("POST", "/start", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(JSON.stringify(x));
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(inputSample));
 });
+
+
+
+
+
+//  function newGame(input){
+//     let gridString = input.substr(0,3);
+//     let instructions = input.split('\n');
+//     let nRobots= (instructions.length -1)/2;
+   
+//     let game = {
+//         grid: {
+//             x: parseInt(gridString[0]),
+//             y: parseInt(gridString[2])
+//         },
+//         robots: []
+//     };
+
+//     for (let robotIdx = 1; robotIdx <= nRobots; robotIdx++) {
+//         let instructionR = instructions[robotIdx*2];
+//         let positionR = instructions[(robotIdx*2)-1];
+//         game.robots.push()
+//         let robot = new Robot(robotIdx, positionR, instructionR);
+//     }
+//     return game;
+//  }
+
