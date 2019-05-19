7c6941fa7f305f2f58c29ae1a5fb20a399e1da73
Laura Corbi Villalba <lcorbivi@everis.com>
2019-05-19 21:59:26 +0200

    working with input

--- public/main.js
+++ public/main.js
@@ -15,50 +15,22 @@ let output = document.getElementById('output');
 //     "value": "5 3\n1 1 E\nRFRFRFRF\n3 2 N \nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL"
 // }
 
-let inputSample = {value: '5 3\n1 1 E\nRFRFRFRF\n3 2 N \nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL'}
+input.value = '5 3\n1 1 E\nRFRFRFRF\n3 2 N \nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL';
 
-input.value = inputSample.value;
 
 
 startButton.addEventListener("click", function(){
-    // let game = newGame(input.value);
-    // console.log(game)
+    let inputObj = {
+        value: input.value
+    }
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          console.log(this.response)
           let response = xhttp.responseText;
           output.value = response;
         }
       };
     xhttp.open("POST", "/start", true);
     xhttp.setRequestHeader("Content-Type", "application/json");
-    xhttp.send(JSON.stringify(inputSample));
+    xhttp.send(JSON.stringify(inputObj));
 });
-
-
-
-
-
-//  function newGame(input){
-//     let gridString = input.substr(0,3);
-//     let instructions = input.split('\n');
-//     let nRobots= (instructions.length -1)/2;
-   
-//     let game = {
-//         grid: {
-//             x: parseInt(gridString[0]),
-//             y: parseInt(gridString[2])
-//         },
-//         robots: []
-//     };
-
-//     for (let robotIdx = 1; robotIdx <= nRobots; robotIdx++) {
-//         let instructionR = instructions[robotIdx*2];
-//         let positionR = instructions[(robotIdx*2)-1];
-//         game.robots.push()
-//         let robot = new Robot(robotIdx, positionR, instructionR);
-//     }
-//     return game;
-//  }
-
